ec8e024a9764460bd0e5a9a4eb8fa89d85be154c
alex <vastagox@gmail.com>
2024-04-01 03:44:52 -0500

    hypr:fix script crashing hyprland

--- .config/hypr/scripts/togglefullscreen.sh
+++ .config/hypr/scripts/togglefullscreen.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 mpv_socket_dir="/tmp/mpvSockets"
 
+# Function to control mpv playback
 mpvplaycontrol() {
     jq -r '.[] | select(.class == "mpv") | "\(.address) \(.pid)"' <<< "$2" | while read -r address pid; do 
-        if [ "$address" == "$1" ]; then
-            echo '{ "command": ["set_property", "pause", false] }' | socat - UNIX-CONNECT:"$mpv_socket_dir/$pid"
-
-        else
-            echo '{ "command": ["set_property", "pause", true] }' | socat - UNIX-CONNECT:"$mpv_socket_dir/$pid"
-        fi
+        if [[ "$address" == "$1" ]]; then mpv_action="false"; else mpv_action="true"; fi
+        echo '{ "command": ["set_property", "pause", '"$mpv_action"'] }' | socat - UNIX-CONNECT:"$mpv_socket_dir/$pid"
     done
 }
 
 clients=$(hyprctl clients -j)
 mapfile -t addresses_positions < <(jq -r '[.[] | select(.class == "mpv" or .fullscreen == true)] | sort_by(.at[1]) | .[] | "\(.address) \(.at[1])"' <<< "$clients")  
 
-if [[ "${#addresses_positions[@]}" -eq 0 ]];then
-    exit
-fi
+[[ "${#addresses_positions[@]}" -eq 0 ]] && exit
 
-read -r next_address height <<< "${addresses_positions[0]}"
+read -r target_address y_coord <<< "${addresses_positions[0]}"
 
-if [[ "$height" -eq 0 ]]; then
-    hypr_cmd="dispatch fullscreen; dispatch pin address:$next_address;"
+if [[ "$y_coord" -eq 0 ]]; then
+    hypr_cmd="dispatch fullscreen; dispatch pin address:$target_address"
+	addresses_positions=("${addresses_positions[@]:1}")
 
-	if [[ ${#addresses_positions[@]} -gt 1 ]];then
-	    addresses_positions=("${addresses_positions[@]:1}")
+	if [[ ${#addresses_positions[@]} -gt 0 ]];then
         if [[ ${addresses_positions[0]#* } -eq 20 && ${addresses_positions[1]#* } -eq 620 ]]; then
-	        next_address=${addresses_positions[1]% *}
+	        target_address=${addresses_positions[1]% *}
         else
-	        next_address=${addresses_positions[0]% *}
+	        target_address=${addresses_positions[0]% *}
         fi
-        mpvplaycontrol "$next_address" "$clients"
     else
-        hyprctl --batch ""$hypr_cmd" dispatch focuscurrentorlast"
-        exit
+        next_workspace=$(jq -r '.[] | select(.address == "'"${target_address}"'") | .workspace.id' <<< "$clients")
+        jq -e --argjson workspace "$next_workspace" '.[] | select(.workspace.id == $workspace and .class != "mpv")' <<< "$clients" >/dev/null && hypr_cmd+="; dispatch focuscurrentorlast"
 	fi
 fi
 
-hypr_cmd+="dispatch focuswindow address:$next_address; dispatch pin address:$next_address; dispatch fullscreen"
+if [[ ${#addresses_positions[@]} -gt 0 ]]; then
+    mpvplaycontrol "$target_address" "$clients"
+    hypr_cmd+=";dispatch focuswindow address:$target_address; dispatch pin address:$target_address; dispatch fullscreen"
+fi
 hyprctl --batch "$hypr_cmd"
