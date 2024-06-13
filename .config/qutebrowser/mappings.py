78c27ba387113944dab458213cac607839c12af5
edwin <vastagox@gmail.com>
2024-06-13 18:41:19 -0500

    Add toggle darkmode Qutebrowser, udiskie

--- .config/qutebrowser/mappings.py
+++ .config/qutebrowser/mappings.py
@@ -29,6 +29,7 @@ bind = {
 
 	leader + "js": "config-cycle content.javascript.enabled true false",
 	leader + "b": "config-cycle tabs.show switching always",
+	leader + leader: "config-cycle colors.webpage.darkmode.enabled true false",
 	leader + "v": "config-source ;; message-info 'qutebrowser reloaded'",
     ## mpv 
     lleader + "m": "hint videos userscript qute-mpv",
