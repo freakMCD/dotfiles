56cd585cd9c4861285d308b9b928f8e2753cbef2
edwin <vastagox@gmail.com>
2024-10-06 08:55:34 -0500

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -150,6 +150,7 @@ vim.g.matchup_delim_start_plaintext = 0
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_syntax_conceal_disable = 1
 vim.g.vimtex_quickfix_open_on_warning = 0  
+vim.g.vimtex_quickfix_autoclose_after_keystrokes = 10
 vim.g.vimtex_imaps_enabled = 0
 vim.g.vimtex_delim_stopline = 50
 vim.g.vimtex_compiler_latexmk = {
