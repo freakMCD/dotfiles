0f7b664a965231b2f03a450ef75d4bbc39fe4062
edwin <vastagox@gmail.com>
2024-11-12 08:01:21 -0500

    Update

--- .config/hypr/scripts/variables.sh
+++ .config/hypr/scripts/variables.sh
@@ -8,11 +8,11 @@ y_offset=-100
 l_hidden=$(($x_offset-$mpv_width))
 r_hidden=$((1920-$x_offset))
 # mpv coords
-x2_coord=$((1920-$x_offset-$mpv_width))
-y2_coord=$y_offset
+x1_coord=$((1920-$x_offset-$mpv_width))
+y1_coord=$y_offset
 
-x1_coord=$x2_coord
-y1_coord=$((1080-$y_offset-$mpv_height))
+x2_coord=$x1_coord
+y2_coord=$((1080-$y_offset-$mpv_height))
 
 x3_coord=$x_offset
 y3_coord=$y1_coord
