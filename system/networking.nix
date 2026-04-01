fff0db82b73356a7664188360b319e4b4f05f177
Alex <vastagox@gmail.com>
2026-03-31 19:02:43 -0500

    Add nextdns again

--- system/networking.nix
+++ system/networking.nix
@@ -1,5 +1,12 @@
 {
+  services.nextdns = {
+    enable = true;
+    arguments = [
+      "-config" "4457ba"
+      "-report-client-info"
+    ];
+  };
+  networking.nameservers = ["127.0.0.1" "::1"];
   networking.useDHCP = true;
-  networking.useNetworkd = true;
 }
 
