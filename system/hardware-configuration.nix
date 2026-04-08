634bfc19bec80022ad846d144ebb326fba1b66fa
Alex <vastagox@gmail.com>
2026-04-08 00:48:43 -0500

    Enable hibernation with swapfile and hypridle updates

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -30,7 +30,12 @@
       options = [ "noatime" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16*1024;
+    }
+  ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
